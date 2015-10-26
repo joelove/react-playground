390c3c904ca6c778456e80b09ab17f84ff28647d
Joe Love <git@joelove.uk>
2015-10-26 16:05:27 +0000

    updated app to use api to serve comments

--- entry.js
+++ entry.js
@@ -1,14 +1,47 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-
-const data = [
-  {title: 'My fancy title', text: 'This is one comment'},
-  {title: 'Another title', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'},
-];
+import React from 'react';
+import ReactDOM from 'react-dom';
+import fetch from 'isomorphic-fetch';
 
 class CommentBox extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {data: []};
+  }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  }
+
+  loadCommentsFromServer = () => {
+    fetch(this.props.url).then(response => {
+      if (response.status >= 400) {
+        throw new Error('Bad response from server');
+      }
+      return response.json();
+    }).then(data => {
+      this.setState({data: data});
+    });
+  }
+
+  handleCommentSubmit = (comment) => {
+    fetch(this.props.url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(comment),
+    }).then(response => {
+      if (response.status >= 400) {
+        throw new Error('Bad response from server');
+      }
+    });
+  }
+
   render() {
-    const commentNodes = this.props.data.map(function(comment) {
+    const commentNodes = this.state.data.map(comment => {
       return (
         <div className="commentBox">
           <CommentTitle title={comment.title} />
@@ -19,6 +52,7 @@ class CommentBox extends React.Component {
     return (
       <div className="commentList">
         {commentNodes}
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
       </div>
     );
   }
@@ -45,7 +79,32 @@ class CommentText extends React.Component {
   }
 }
 
+class CommentForm extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const title = this.refs.title.value.trim();
+    const text = this.refs.text.value.trim();
+
+    if (!text || !title) {
+      return;
+    }
+
+    this.props.onCommentSubmit({title: title, text: text});
+  }
+
+  render() {
+    return (
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Title" ref="title" />
+        <input type="text" placeholder="Say something..." ref="text" />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+}
+
 ReactDOM.render(
-  <CommentBox data={data} />,
+  <CommentBox url="http://localhost:4730" pollInterval={2000} />,
   document.getElementById('content')
 );
